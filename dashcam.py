fa37d58ab041cb4d1af25184f34e952035f03d23
Gareth <garethjsharpe@gmail.com>
2018-02-23 03:09:36 -0500

    cron script on reboot

--- dashcam.py
+++ dashcam.py
@@ -4,28 +4,30 @@ import datetime
 
 import dashcam_utilities as util
 
-if not os.path.exists("Video"):
-	os.makedirs("Video")
+if not os.path.exists("/home/pi/dashcam/Video"):
+	os.makedirs("/home/pi/dascam/Video")
 
-if not os.path.exists("Video/January"):
-	util.make_dirs("Video")
+if not os.path.exists("/home/pi/dashcam/Video/January"):
+	util.make_dirs("/home/pi/dashcam/Video")
 
-size = get_dir_size("Video")
+size = util.get_dir_size("/home/pi/dashcam/Video")
 
-if size >= 10:
+if size >= 7.5:
 	util.rm_dirs("Video")
 	util.make_dirs("Video")
 
-camera = get_camera()
+camera = util.get_camera()
 now = datetime.datetime.now()
 month = calendar.month_name[now.month]
-path = os.join(month, now) + util.ext
+path = os.path.join("/home/pi/dashcam/Video", month, now.strftime('%Y-%m-%d %H:%M:%S')) + util.ext
 camera.start_recording(path)
+# camera.start_preview()
 
 try:
-	while true:
+	while True:
 		now = datetime.datetime.now()
 		camera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S')
 		camera.wait_recording(0.2)
 finally:
 	camera.stop_recording()
+	# camera.stop_preview()
--- dashcam_utilities.py
+++ dashcam_utilities.py
@@ -2,17 +2,18 @@ import os
 import sys
 import shutil
 import picamera
+import calendar
 
 ext = ".h264"
 
 def get_dir_size(start_path='.'):
     total_size = 0
-    bytes_in_gigabyte = 1024 * 1024.0
+    bytes_in_gigabyte = 1024 * 1024 * 1024
     for dirpath, dirnames, filenames in os.walk(start_path):
         for file in filenames:
             fp = os.path.join(dirpath, file)
             total_size += os.path.getsize(fp)
-    return total_size / bytes_in_gigabyte 
+    return total_size / bytes_in_gigabyte
 
 def rm_dirs(start_path='.'):
 	for dirpath, dirnames, filenames in os.walk(start_path):
@@ -20,11 +21,12 @@ def rm_dirs(start_path='.'):
 
 def make_dirs(start_path='.'):
 	for i in range(1, 13):
-		directory = os.path.join(start_path, calandar.month_name[i])
+		directory = os.path.join(start_path, calendar.month_name[i])
 		if not os.path.exists(directory):
 			os.makedirs(directory)
 
 def get_camera():
-	camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
+	camera = picamera.PiCamera(resolution=(640, 480), framerate=24)
 	camera.annotate_background = picamera.Color('black')
+	camera.rotation = 270
 	return camera
